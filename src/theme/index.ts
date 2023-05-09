c634ee96cebf06a609bb857fe6be1c6c7701c602
Alefe Bispo <alefebsp@hotmail.com>
2023-05-09 16:14:44 -0300

    installed styled components and phospor icons defined initial Home styles

---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    GRAY_100: '#999999',
+    GRAY_200: '#777777',
+    GRAY_300: '#555555',
+    BLUE_100: '#90e0ef',
+    BLUE_200: '#48cae4',
+    BLUE_300: '#00b4d8',
+    BLUE_400: '#023e8a',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto-Regular',
+    BOLD: 'Roboto-Regular',
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
