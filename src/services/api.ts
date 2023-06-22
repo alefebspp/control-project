644a05d49d40b85273733e72c0c5efd265867cb3
Alefe Bispo <alefebsp@hotmail.com>
2023-06-22 17:01:00 -0300

    defined AppError and api interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,20 @@
 import axios from 'axios';
 import {API_SERVER_URL} from '@env';
+import {AppError} from '../utils/AppError';
 
 export const api = axios.create({
   baseURL: API_SERVER_URL,
 });
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(
+        new AppError('Error no servidor.Tente novamente mais tarde'),
+      );
+    }
+  },
+);
