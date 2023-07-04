9910a7c45139884a33688fecd1d337c052f43d12
Alefe Bispo <alefebsp@hotmail.com>
2023-07-04 16:46:10 -0300

    defined way of route be authenticaded or not

--- src/routes/interface.ts
+++ src/routes/interface.ts
@@ -1,15 +1,16 @@
 import {RouteProp} from '@react-navigation/native';
-import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 type RootStackParamList = {
   home: undefined;
   login: undefined;
+  adjustments: undefined;
   registry: {
     registryDate?: Date | undefined;
   };
 };
 
-export type NavigationProps = BottomTabNavigationProp<RootStackParamList>;
+export type NavigationProps = NativeStackNavigationProp<RootStackParamList>;
 export type RoutesProps = RouteProp<RootStackParamList>;
 export type RouteProps<Route extends keyof RootStackParamList> = RouteProp<
   RootStackParamList,
