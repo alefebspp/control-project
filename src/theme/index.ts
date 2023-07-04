1aed1e455019b67321862f22aa293985b65544a6
Alefe Bispo <alefebsp@hotmail.com>
2023-07-04 16:45:10 -0300

    defined new themes and toastConfig

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,7 @@
 export default {
   COLORS: {
     WHITE: '#FFFFFF',
+    GRAY_50: '#ececec',
     GRAY_100: '#d3d3d3',
     GRAY_200: '#777777',
     GRAY_300: '#555555',
@@ -9,12 +10,15 @@ export default {
     BLUE_300: '#00b4d8',
     BLUE_400: '#023e8a',
     ERROR: '#FF0000',
+    PENDING: '#FFD580',
+    ACCEPTED: '#32CD32',
   },
   FONT_FAMILY: {
     REGULAR: 'Roboto-Regular',
     BOLD: 'Roboto-Bold',
   },
   FONT_SIZE: {
+    XS: 10,
     SM: 14,
     MD: 16,
     LG: 18,
