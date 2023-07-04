c83caa0e45a89c60da66f2314c24880ccf52c214
Alefe Bispo <alefebsp@hotmail.com>
2023-07-04 16:46:20 -0300

    defined index of components folder

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export {Clock} from './Clock';
+export {ModalDatePicker} from './ModalDatePicker';
+export {DatePicker} from './DatePicker';
+export {Input} from './Input';
+export {Header} from './Header';
+export {Empty} from './Empty';
+export {Adjustment} from './Adjustment';
