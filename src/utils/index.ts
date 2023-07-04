ce3851daee0f3dfe6e621aac463303eb9132f5e1
Alefe Bispo <alefebsp@hotmail.com>
2023-07-04 16:45:43 -0300

    defined new utils functions and schema

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,7 @@ export {getHourMinutesFormated} from './getHourMinutesFormated';
 export {formatDateToDayMonth} from './formatDateToDayMonth';
 export {resetDateTime} from './resetDateTime';
 export {monthMap} from './monthMap';
+export {checkIfCurrentDateEqualsRegistryDate} from './checkIfCurrencDateEqualsRegistryDate';
+export {convertStatusLabel} from './adjustmentStatusConverter';
+export {convertRegistryType} from './registryTypeConverter';
+export {loginSchema, requestSchema} from './schemas';
