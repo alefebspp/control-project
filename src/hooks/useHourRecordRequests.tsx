428492c6608771bb6cb9951b1bdbd22627e9dcc6
Alefe Bispo <alefebsp@hotmail.com>
2023-09-25 12:11:15 -0300

    defined hour record requests

---
+++ src/hooks/useHourRecordRequests.tsx
@@ -0,0 +1,22 @@
+import {useQuery} from '@tanstack/react-query';
+import {getCollaboratorHourRecords} from '../services/HourRecord';
+
+export const useHourRecordRequests = () => {
+  const find = (collaborator_id?: string, period?: string) => {
+    const {data, isLoading, refetch} = useQuery({
+      queryKey: ['hourRecord', period],
+      queryFn: () => getCollaboratorHourRecords(collaborator_id, period),
+      enabled: false,
+    });
+
+    return {
+      data,
+      isLoading,
+      refetch,
+    };
+  };
+
+  return {
+    find,
+  };
+};
---
+++ src/services/HourRecord/index.ts
@@ -0,0 +1,1 @@
+export {getCollaboratorHourRecords} from './GetCollaboratorHourRecords';
