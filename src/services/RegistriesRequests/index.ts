62d73856c21397bd3d2223b92f229eaad9e80a56
Alefe Bispo <alefebsp@hotmail.com>
2023-09-25 12:15:54 -0300

    removed list registries and calculate registries hours

--- src/services/RegistriesRequests/index.ts
+++ src/services/RegistriesRequests/index.ts
@@ -1,5 +1,3 @@
-export {ListRegistriesRequest} from './ListRegistriesRequest';
 export {findCollaboratorRegistries} from './FindCollaboratorRegistriesRequest';
 export {updateRegistryRequest} from './UpdateRegistryRequest';
 export {createRegistryRequest} from './CreateRegistryRequest';
-export {calculateRegistriesHours} from './CalculateRegistriesHours';
